e992d751c6693dfa07428486860fe70867e3dbc4
zhongpeng <zhongpeng2000@gmail.com>
2016-08-09 23:26:22 -0700

    cart initial

---
+++ SportsStore.UnitTests/CartTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SportsStore.Domain.Entities;
+using System.Linq;
+
+
+namespace SportsStore.UnitTests
+{
+    [TestClass]
+    public class CartTest
+    {
+        [TestMethod]
+        public void Can_Add_New_Lines()
+        {
+            //Arrange
+            Product p1 = new Product { ProductID = 1, Name = "P1" };
+            Product p2 = new Product { ProductID = 2, Name = "P1" };
+
+            Cart target = new Cart();
+
+            //Act
+            target.AddItem(p1, 1);
+            target.AddItem(p2, 1);
+            Cart.CartLine[] result = target.Lines.ToArray();
+
+            //Assert
+            Assert.AreEqual(result.Length, 2);
+            Assert.AreEqual(result[0].Product, p1);
+            Assert.AreEqual(result[1].Product, p2);
+        }
+
+        [TestMethod]
+        public void Can_Add_Quantity_To_Existing_Line()
+        {
+            //Arrange
+            Product p1 = new Product { ProductID = 1, Name = "P1" };
+            Product p2 = new Product { ProductID = 2, Name = "P1" };
+
+            Cart target = new Cart();
+
+            //Act
+            target.AddItem(p1, 1);
+            target.AddItem(p2, 1);
+            target.AddItem(p1, 10);
+            Cart.CartLine[] result = target.Lines.ToArray();
+
+            //Assert
+            Assert.AreEqual(result.Length, 2);
+            Assert.AreEqual(result[0].Product, p1);
+            Assert.AreEqual(result[1].Product, p2);
+            Assert.AreEqual(result[0].Quantity, 11);
+            Assert.AreEqual(result[1].Quantity, 1);
+        }
+    }
+}
