874201d92f65fe8513733e2250db5e395cf25f9f
zhongpeng <zhongpeng2000@gmail.com>
2016-08-14 13:17:05 -0700

    cart controller

---
+++ SportsStore.Domain/Entities/CartIndexViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsStore.Domain.Entities
+{
+    public class CartIndexViewModel
+    {
+        public Cart cart { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
