efb7f511b149b6c81e5a567597c84fcebd48afac
rache <rache@DESKTOP-DIN6Q5E>
2016-08-20 20:00:17 -0700

    edit

--- SportsStore.Domain/Abstract/IProductRepository.cs
+++ SportsStore.Domain/Abstract/IProductRepository.cs
@@ -12,5 +12,7 @@ namespace SportsStore.Domain.Abstract
     public interface IProductRepository
     {
         IEnumerable<Product> Products { get; }
+
+        void SaveProduct(Product product);
     }
 }
--- SportsStore.WebUI/Controllers/AdminController.cs
+++ SportsStore.WebUI/Controllers/AdminController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
 
 namespace SportsStore.WebUI.Controllers
 {
@@ -19,6 +20,27 @@ namespace SportsStore.WebUI.Controllers
         public ViewResult Index()
         {
             return View(repository.Products);
+        }
+
+        public ViewResult Edit(int productId)
+        {
+            Product product = repository.Products.FirstOrDefault(p => p.ProductID == productId);
+            return View(product);
+        }
+        [HttpPost]
+        public ActionResult Edit(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                repository.SaveProduct(product);
+                TempData["message"] = string.Format("{0} has been saved", product.Name);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                //this is something wrong with the data views
+                return View(product);
+            }
         }
     }
 }
